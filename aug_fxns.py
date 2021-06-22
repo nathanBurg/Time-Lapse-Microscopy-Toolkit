b71c18e12d8c28c1f13c3b1f1b4ea42bfe267acf
nathanBurg <nathandburg@gmail.com>
2021-06-22 09:14:02 -0700

    Update aug_fxns.py

--- aug_fxns.py
+++ aug_fxns.py
@@ -3,19 +3,25 @@ import albumentations as albu
 
 def get_training_augmentation():
     train_transform = [
-
         albu.HorizontalFlip(p=0.5),
-
-        albu.Rotate (limit=90, interpolation=1, border_mode=4, value=None, mask_value=None, always_apply=True, p=0.5),
-
-        albu.ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0.1, p=1, border_mode=0),
-
-        albu.PadIfNeeded(min_height=320, min_width=320, always_apply=True, border_mode=0),
+        albu.Rotate(
+            limit=90,
+            interpolation=1,
+            border_mode=4,
+            value=None,
+            mask_value=None,
+            always_apply=True,
+            p=0.5,
+        ),
+        albu.ShiftScaleRotate(
+            scale_limit=0.5, rotate_limit=0, shift_limit=0.1, p=1, border_mode=0
+        ),
+        albu.PadIfNeeded(
+            min_height=320, min_width=320, always_apply=True, border_mode=0
+        ),
         albu.RandomCrop(height=320, width=320, always_apply=True),
-
         albu.IAAAdditiveGaussianNoise(p=0.2),
         albu.IAAPerspective(p=0.5),
-
         albu.OneOf(
             [
                 albu.CLAHE(p=1),
@@ -24,7 +30,6 @@ def get_training_augmentation():
             ],
             p=0.9,
         ),
-
         albu.OneOf(
             [
                 albu.IAASharpen(p=1),
@@ -33,7 +38,6 @@ def get_training_augmentation():
             ],
             p=0.9,
         ),
-
         albu.OneOf(
             [
                 albu.RandomContrast(p=1),
