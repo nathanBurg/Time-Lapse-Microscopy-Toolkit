66b9654df84ae48569018f71f12af63df407b112
nathanBurg <nathandburg@gmail.com>
2021-06-22 09:14:30 -0700

    Update to_grayscale.py

--- to_grayscale.py
+++ to_grayscale.py
@@ -1,30 +1,33 @@
 import cv2
 
-import os,glob
+import os, glob
 
-from os import listdir,makedirs
+from os import listdir, makedirs
 
-from os.path import isfile,join
-path = 'masks_wo_gal_071417_4x_aug_gs' # Source Folder
-dstpath = 'masks_wo_gal_071417_4x_aug' # Destination Folder
+from os.path import isfile, join
+
+path = "masks_wo_gal_071417_4x_aug_gs"  # Source Folder
+dstpath = "masks_wo_gal_071417_4x_aug"  # Destination Folder
 try:
     makedirs(dstpath)
 except:
-    print ("Directory already exist, images will be written in same folder")
+    print("Directory already exist, images will be written in same folder")
 # Folder won't used
-files = list(filter(lambda f: isfile(join(path,f)), listdir(path)))
+files = list(filter(lambda f: isfile(join(path, f)), listdir(path)))
 for image in files:
     try:
-        img = cv2.imread(os.path.join(path,image))
-        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        dstPath = join(dstpath,image)
-        cv2.imwrite(dstPath,gray)
+        img = cv2.imread(os.path.join(path, image))
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        dstPath = join(dstpath, image)
+        cv2.imwrite(dstPath, gray)
     except:
-        print ("{} is not converted".format(image))
+        print("{} is not converted".format(image))
 for fil in glob.glob("*.tif"):
     try:
         image = cv2.imread(fil)
-        gray_image = cv2.cvtColor(os.path.join(path,image), cv2.COLOR_BGR2GRAY) # convert to greyscale
-        cv2.imwrite(os.path.join(dstpath,fil),gray_image)
+        gray_image = cv2.cvtColor(
+            os.path.join(path, image), cv2.COLOR_BGR2GRAY
+        )  # convert to greyscale
+        cv2.imwrite(os.path.join(dstpath, fil), gray_image)
     except:
-        print('{} is not converted')
+        print("{} is not converted")
